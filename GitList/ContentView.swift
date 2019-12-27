7787e836db7d5815972f16593e5888670b13ea9a
Daniel Watson <d.m.watson1985@gmail.com>
2019-12-27 17:23:59 +0000

    Initial Commit

---
+++ GitList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitList
+//
+//  Created by Daniel Watson on 27/12/2019.
+//  Copyright © 2019 Daniel Watson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
